43e9254d18bc3253a6337c5f3f9ce3d095f01785
Yamith Romero <yamithr@YamithMSI-deb.yjra.net>
2026-07-29 16:29:54 -0500

    fix: use .ico for taskbar/window icons on Windows

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, nativeImage } = require('electron');
 const path = require('path');
 
 if (process.platform === 'win32') {
@@ -6,17 +6,26 @@ if (process.platform === 'win32') {
 }
 
 function createWindow() {
+  const iconPath = path.join(__dirname, 'assets', 'icon.ico');
+  let winIcon;
+  try {
+    winIcon = nativeImage.createFromPath(iconPath);
+    if (winIcon.isEmpty()) winIcon = undefined;
+  } catch {
+    winIcon = undefined;
+  }
+
   const win = new BrowserWindow({
     width: 1000,
     height: 720,
     minWidth: 720,
     minHeight: 520,
+    icon: winIcon || path.join(__dirname, 'assets', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true
-    },
-    icon: path.join(__dirname, 'assets', 'icon.png')
+    }
   });
 
   win.loadFile('index.html');
