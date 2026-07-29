d1e732396eef56830303818a17b929fe5b702605
Yamith Romero <yamithr@YamithMSI-deb.yjra.net>
2026-07-29 15:36:07 -0500

    feat: add MSI Windows installer with taskbar pinning support

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+if (process.platform === 'win32') {
+  app.setAppUserModelId('com.yamithr.etc-calculator');
+}
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
